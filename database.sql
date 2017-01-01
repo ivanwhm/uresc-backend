e28b2dcd8445a881ca172be884a6a97c40919f1c
Ivan Wilhelm <ivan.whm@me.com>
2017-01-01 11:31:29 -0200

    Fix user table ID column Add tables calendar and event

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `user` (
-`user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 `name` varchar(100) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
 `email` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
 `username` varchar(255) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
@@ -10,11 +10,11 @@ CREATE TABLE `user` (
 `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `user_created` int(10) unsigned DEFAULT NULL,
 `user_updated` int(10) unsigned DEFAULT NULL,
-PRIMARY KEY (`user_id`),
+PRIMARY KEY (`id`),
 KEY `idx_user_user_created` (`user_created`),
 KEY `idx_user_user_updated` (`user_updated`),
-CONSTRAINT `fk_user_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_user_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+CONSTRAINT `fk_user_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_user_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `user_access` (
@@ -25,8 +25,8 @@ CREATE TABLE `user_access` (
 `type` char(1) COLLATE utf8_swedish_ci NOT NULL DEFAULT 'C',
 PRIMARY KEY (`user_access_id`),
 KEY `IDX_USER_ACCESS_USER_ID` (`user_id`),
-CONSTRAINT `FK_USER_ACCESS_USER_USER_ID` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+CONSTRAINT `FK_USER_ACCESS_USER_USER_ID` FOREIGN KEY (`user_id`) REFERENCES `USER` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `department` (
 `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -40,14 +40,14 @@ CREATE TABLE `department` (
 PRIMARY KEY (`id`),
 KEY `idx_department_user_created` (`user_created`),
 KEY `idx_department_user_updated` (`user_updated`),
-CONSTRAINT `fk_department_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_department_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+CONSTRAINT `fk_department_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_department_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `download_category` (
 `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 `name` varchar(59) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
-`status` char(1) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
+`status` char(1) COLLATE utf8_swedish_ci NOT NULL DEFAULT 'A',
 `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `user_created` int(10) unsigned NOT NULL,
@@ -55,8 +55,8 @@ CREATE TABLE `download_category` (
 PRIMARY KEY (`id`),
 KEY `idx_download_category_user_created` (`user_created`),
 KEY `idx_download_category_user_updated` (`user_updated`),
-CONSTRAINT `fk_download_category_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_download_category_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+CONSTRAINT `fk_download_category_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_download_category_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `download` (
@@ -74,9 +74,9 @@ KEY `idx_download_category_id` (`category_id`),
 KEY `idx_download_user_created` (`user_created`),
 KEY `idx_download_user_updated` (`user_updated`),
 CONSTRAINT `fk_download_category_id` FOREIGN KEY (`category_id`) REFERENCES `download_category` (`id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_download_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_download_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+CONSTRAINT `fk_download_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_download_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `gallery_category` (
 `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -89,9 +89,9 @@ CREATE TABLE `gallery_category` (
 PRIMARY KEY (`id`),
 KEY `Idx_gallery_category_user_created` (`user_created`),
 KEY `Idx_gallery_category_user_updated` (`user_updated`),
-CONSTRAINT `fk_gallery_category_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_gallery_category_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+CONSTRAINT `fk_gallery_category_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_gallery_category_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 CREATE TABLE `gallery` (
 `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -108,6 +108,43 @@ KEY `idx_gallery_category_id` (`category_id`),
 KEY `idx_gallery_user_created` (`user_created`),
 KEY `idx_gallery_user_updated` (`user_updated`),
 CONSTRAINT `fk_gallery_category_id` FOREIGN KEY (`category_id`) REFERENCES `gallery_category` (`id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_gallery_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_gallery_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+CONSTRAINT `fk_gallery_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_gallery_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+CREATE TABLE `calendar` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`name` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+`status` char(1) COLLATE utf8_swedish_ci NOT NULL DEFAULT 'A',
+`date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`user_created` int(10) unsigned NOT NULL,
+`user_updated` int(10) unsigned NOT NULL,
+PRIMARY KEY (`id`),
+KEY `idx_calendar_user_created` (`user_created`),
+KEY `idx_calendar_user_updated` (`user_updated`),
+CONSTRAINT `fk_calendar_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_calendar_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+CREATE TABLE `event` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`calendar_id` int(11) unsigned NOT NULL,
+`name` varchar(100) COLLATE utf8_swedish_ci NOT NULL DEFAULT '',
+`date` date NOT NULL,
+`start_time` time NOT NULL,
+`end_time` time NOT NULL,
+`info` text COLLATE utf8_swedish_ci,
+`date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`user_created` int(10) unsigned NOT NULL,
+`user_updated` int(10) unsigned NOT NULL,
+PRIMARY KEY (`id`),
+KEY `idx_event_calendar_id` (`calendar_id`),
+KEY `idx_event_user_created` (`user_created`),
+KEY `idx_event_user_updated` (`date_updated`),
+KEY `fk_event_user_updated` (`user_updated`),
+CONSTRAINT `fk_event_calendar_id` FOREIGN KEY (`calendar_id`) REFERENCES `calendar` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_event_user_created` FOREIGN KEY (`user_created`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `fk_event_user_updated` FOREIGN KEY (`user_updated`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
